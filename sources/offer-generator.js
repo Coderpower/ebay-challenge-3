8734517fd3e4491b90ad3f2ddd3450dba904edff
Julien Sarazin <julien.sarazin@me.com>
2016-06-15 13:05:22 +0200

    finalized challenge. waiting for QC

---
+++ sources/offer-generator.js
@@ -0,0 +1,3 @@
+module.exports = function(client, products){
+    return [];
+};
--- test/offer-generator.specs.js
+++ test/offer-generator.specs.js
@@ -11,9 +11,21 @@ suite('Offer Generator', function(){
         var offers  = getOffers(client, products);
 
         expect(offers).to.eql([
-            "575fdb7fc33f3e6505479ec8",
-            "575fdb7f625ab938a53d92e6",
-            "575fdb7f2962a7d577a6d06d"
+            {
+                "price": 319.99,
+                "productId": "575fdb7fc33f3e6505479ec8",
+                "relevance": 4
+            },
+            {
+                "price": 699.99,
+                "productId": "575fdb7f625ab938a53d92e6",
+                "relevance": 3
+            },
+            {
+                "price": 90,
+                "productId": "575fdb7f2962a7d577a6d06d",
+                "relevance": 2.5
+            }
         ]);
     });
 
@@ -24,14 +36,41 @@ suite('Offer Generator', function(){
         var offers  = getOffers(client, products);
 
         expect(offers).to.eql([
-            "575fdb7f625ab938a53d181f",
-            "575fdb7fd848ed0f63ebbaf4",
-            "575fdb7f625ab938a53d92e6",
-            "575fdb7f2082640b0256e704",
-            "575fdb7f0e8152b40b1f0e67",
-            "575fdb7f5d7b6bbe45ca84cb",
-            "575fdb7fc33f3e6505479ec8",
-
+            {
+                "price": 8399.99,
+                "productId": "575fdb7f625ab938a53d181f",
+                "relevance": 6
+            },
+            {
+                "price": 170.99,
+                "productId": "575fdb7fd848ed0f63ebbaf4",
+                "relevance": 4
+            },
+            {
+                "price": 699.99,
+                "productId": "575fdb7f625ab938a53d92e6",
+                "relevance": 4
+            },
+            {
+                "price": 15,
+                "productId": "575fdb7f2082640b0256e704",
+                "relevance": 2
+            },
+            {
+                "price": 39.99,
+                "productId": "575fdb7f0e8152b40b1f0e67",
+                "relevance": 2
+            },
+            {
+                "price": 55.99,
+                "productId": "575fdb7f5d7b6bbe45ca84cb",
+                "relevance": 2
+            },
+            {
+                "price": 319.99,
+                "productId": "575fdb7fc33f3e6505479ec8",
+                "relevance": 1.3
+            }
         ]);
     });
 
@@ -42,7 +81,11 @@ suite('Offer Generator', function(){
         var offers  = getOffers(client, products);
 
         expect(offers).to.eql([
-            "575fdb7fc33f3e6505479ec8"
+            {
+                "price": 319.99,
+                "productId": "575fdb7fc33f3e6505479ec8",
+                "relevance": 1.3
+            }
         ]);
     });
 });
