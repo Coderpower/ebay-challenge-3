eed61d9d8bd23644c8cfa5c331607d4f5f96f892
Julien Sarazin <julien.sarazin@me.com>
2016-06-15 12:08:43 +0200

    update tests and initial data

--- test/offer-generator.specs.js
+++ test/offer-generator.specs.js
@@ -1,32 +1,48 @@
 var expect = require('expect.js');
-var addition = require('../sources/offer-generator.js');
-
-
-var products = [
-
-]
+var getOffers = require('../sources/offer-generator.js');
+var data = require('./products.json');
+var clients = require('./clients.json');
 
 suite('Offer Generator', function(){
+    test('Should advice Edward with the proper products', function(){
+        var products = JSON.parse(JSON.stringify(data))
+        var client = clients[0];
 
-    test('Must return the right products', function(){
-        var a = 'p';
-        var b = 32;
-        var result = addition(a, b);
-        expect(result).to.be('isNaN');
+        var offers  = getOffers(client, products);
+
+        expect(offers).to.eql([
+            "575fdb7fc33f3e6505479ec8",
+            "575fdb7f625ab938a53d92e6",
+            "575fdb7f2962a7d577a6d06d"
+        ]);
     });
 
-    test('Must return a number', function(){
-        var a = '2';
-        var b = 5;
-        var result = addition(a, b);
-        expect(result).to.be.a('number');
+    test('Should advice Adeline with the proper products', function(){
+        var products = JSON.parse(JSON.stringify(data))
+        var client = clients[1];
+
+        var offers  = getOffers(client, products);
+
+        expect(offers).to.eql([
+            "575fdb7f625ab938a53d181f",
+            "575fdb7fd848ed0f63ebbaf4",
+            "575fdb7f625ab938a53d92e6",
+            "575fdb7f2082640b0256e704",
+            "575fdb7f0e8152b40b1f0e67",
+            "575fdb7f5d7b6bbe45ca84cb",
+            "575fdb7fc33f3e6505479ec8",
+
+        ]);
     });
 
-    test('Must return a right result', function(){
-        var a = '23';
-        var b = 54;
-        var result = addition(a, b);
-        expect(result).to.be.equal(77);
-    });
+    test('Should advice Jack with the proper products', function(){
+        var products = JSON.parse(JSON.stringify(data))
+        var client = clients[2];
 
+        var offers  = getOffers(client, products);
+
+        expect(offers).to.eql([
+            "575fdb7fc33f3e6505479ec8"
+        ]);
+    });
 });
